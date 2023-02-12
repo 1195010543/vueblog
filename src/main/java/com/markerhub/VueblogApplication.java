3e447b417ea96d5f11ad40d5e294f5b2a5898d30
rorry <luolei@haizhi.com>
2023-02-12 16:18:40 +0800

    添加打印

--- src/main/java/com/markerhub/VueblogApplication.java
+++ src/main/java/com/markerhub/VueblogApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VueblogApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(VueblogApplication.class, args);
+        System.out.println("已经执行了启动服务！");
     }
 
 }
